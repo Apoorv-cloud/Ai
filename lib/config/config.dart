4bb74b18858c574838b705535b192142fca08995
Apoorv-cloud <63242652+Apoorv-cloud@users.noreply.github.com>
2022-03-19 13:30:38 +0530

    Changes

---
+++ lib/config/config.dart
@@ -0,0 +1,5 @@
+const String serverProtocol = "https://";
+const String serverUrl =
+    "demo4.traccar.org"; //"tracking.emkatech.tn"; //"demo4.traccar.org";
+//const String monarchTrackApiUrl = "https://tracking.emkatech.tn/api";
+//const String monarchTrackWebUrl = "https://tracking.emkatech.tn";
